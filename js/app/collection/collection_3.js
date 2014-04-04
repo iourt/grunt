3d5abdaa74ae7ff2522db75d3a0763415d3a1620
bear <appuebear@gmail.com>
2014-04-04 18:54:52 +0800

    add collection

--- js/app/collection/collection_3.js
+++ js/app/collection/collection_3.js
@@ -10,7 +10,44 @@ define([
 		// pop()：移除集合尾部的一个模型对象
 		// shift()：移除集合头部的一个模型对象
 
-		
+		// 定义模型类  
+		var Book = Backbone.Model.extend({  
+		    defaults : {  
+		        name : '',  
+		        price : 0  
+		    }  
+		});  
+		  
+		// 定义初始化数据  
+		var data = [{  
+		    name : '构建高性能Web站点',  
+		    price : 56.30  
+		}, {  
+		    name : '深入分析Java Web技术内幕',  
+		    price : 51.80  
+		}, {  
+		    name : '编写高质量代码:Web前端开发修炼之道',  
+		    price : 36.80  
+		}, {  
+		    name : '基于MVC的JavaScript Web富应用开发',  
+		    price : 42.50  
+		}, {  
+		    name : 'RESTful Web Services Cookbook中文版',  
+		    price : 44.30  
+		  
+		}]  
+		  
+		// 创建集合对象  
+		var books = new Backbone.Collection(data, {  
+		    model : Book  
+		});  
+		  
+		books.remove(books.models[2]);  
+		books.pop();  
+		books.shift();  
+		  
+		// 在控制台输出集合中的模型列表  
+		console.dir(books.models); 
 	};
 	return Collection3;
 });
